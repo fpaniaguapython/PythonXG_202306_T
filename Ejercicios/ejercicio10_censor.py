b5fd38f66ebbe02660256b0ef269ac4e233168ab
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2023-06-06 20:31:55 +0200

    Sesión 02

---
+++ Ejercicios/ejercicio10_censor.py
@@ -0,0 +1,4 @@
+import lector_palabras
+
+lineas = lector_palabras.get_lines_from_file("D:/_PythonXunta/PythonXG_202306_T/Ejercicios/lector_palabras.py")
+print(lineas)
--- Ejercicios/lector_palabras.py
+++ Ejercicios/lector_palabras.py
@@ -1,2 +1,11 @@
 #Este módulo lee un fichero compuesto por palabras y devuelve una lista
-nombre = "loquesea"
+def get_lines_from_file(file_name):
+    fichero = open(file_name, mode="r", encoding="utf-8")
+    lineas = fichero.readlines()
+    lineas = [linea.replace('\n','') for linea in lineas]
+    fichero.close()
+    return lineas
+
+if __name__=="__main__":
+    lineas = get_lines_from_file("D:/_PythonXunta/PythonXG_202306_T/Ejercicios/palabras_prohibidas.txt")
+    print(lineas)
